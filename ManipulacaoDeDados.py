f59e806653f1aa8d374697c128abd3dd0c790c48
Wendellmga <wendel.correia@hotmail.com>
2019-04-25 18:10:35 -0300

    teste

---
+++ ManipulacaoDeDados.py
@@ -0,0 +1,16 @@
+num_int = 5
+num_dec = 7.3
+val_str = "qualquer texto"
+
+
+print("O valor é:", num_int)
+print("O valor e: %i" %num_int)
+print("o valor e: " + str(num_int))
+print("")
+print("Concatenando decimal:", num_dec)
+print("Concatenando decimal: %.1f" %num_dec)
+print("Concatenando decimal: " + str(num_dec))
+print("")
+print("Concatenando strings:", val_str)
+print("concatenando strnigs: %s" %val_str)
+print("Concatenando string s:" + val_str)
---
+++ OperacoesMatematicas.py
@@ -0,0 +1,48 @@
+#operações basicas
+""""
+print(10+5)
+print(15*2)
+print(10/6)
+print(10//6)
+print(8-4)
+print(5+4)
+print(10%8)
+"""
+#resto da divisao
+"""
+num_1 = float(input("Digite um numero: "))
+num_2 = float(input("Digite outro numero "))
+
+divisao = num_1 / num_2
+resto = num_1 % num_2
+print()
+print(num_1, "dividido por", num_2, "é igual a: ", divisao)
+print("O resto da divisao de ", num_1, "por", num_2, "é: ",resto)
+"""
+#Potenciação e Radiciação
+"""
+print(9**2)
+print(81**0.5)
+"""
+#Opradores Reacionais
+"""
+print(1==1)
+print(2!=2)
+print('a' == 'b')
+print('a' != 'b')
+
+a = (100!=100)
+print(a)
+
+print(3>5)
+print(3>=2)
+print(78<80)
+print(5<5.1 or 5==5.1)
+print('a'<'b')
+"""
+
+
+
+
+
+
