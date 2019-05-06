c337f541cd946eb6c87dec427c2bf11cb7ab5c88
Wendellmga <wendel.correia@hotmail.com>
2019-05-06 19:36:29 -0300

    Atribuição Condicional

---
+++ AtribuicaoCondicional.py
@@ -0,0 +1,9 @@
+"""
+x = 10
+texto = "sim" if x == 10 else " não"
+print(texto)
+"""
+num1 = int(input("Digite um número: "))
+s = "par" if num1 % 2 == 0 else "ímpar"
+print()
+print("O número digitado é",s)
